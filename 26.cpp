ce9ab9c411eba84f41a0278bd32eafb280ee3b0e
Sakshi Pokhriyal <sakshi.pokhriyal007@gmail.com>
2024-12-11 15:25:28 +0530

    Add files via upload

---
+++ 26.cpp
@@ -0,0 +1,14 @@
+//Example using C style string
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char str1[10]="Hello";
+    char str2[10]="World";
+    cout<<str1<<"\n";
+    cout<<str2;
+    return 0;
+}
